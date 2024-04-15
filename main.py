9742d01b2021682b27ba94a8b95df53de8b84282
adrianoferraro27101995 <adriano.ferraro27101995@gmail.com>
2024-04-15 19:06:38 -0300

    Salvando meu arquivo main novamente

---
+++ main.py
@@ -0,0 +1,18 @@
+print('jornada de dados')
+
+# Variavel
+idade = 28
+nome = "Adriano"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4,5]
+
+# Metodos
+lista_invertida = lista.reverse()
